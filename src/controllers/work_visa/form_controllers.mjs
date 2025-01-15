d830f2ca41772fdbb3eec9c0b26811b7db62311b
albanovs <albvnovs@gmail.com>
2025-01-15 11:23:21 +0600

    bug is done

--- src/controllers/work_visa/form_controllers.mjs
+++ src/controllers/work_visa/form_controllers.mjs
@@ -1,11 +1,20 @@
 import FormModel from "../../models/form_model.mjs";
+import path from "path";
+import crypto from "crypto";
 
+const shortenFileName = (filename) => {
+    const ext = path.extname(filename);
+    const baseName = path.basename(filename, ext);
+    const hash = crypto.createHash('md5').update(baseName).digest('hex').slice(0, 8);
+    return `${baseName.slice(0, 5)}_${hash}${ext}`;
+};
 const localsaveFormData = async (req, res) => {
     try {
         const { body, files } = req;
-        const photo = req.files.photo ? req.files.photo[0].filename : null;
+        const photo = req.files.photo ? shortenFileName(req.files.photo[0].filename) : null;
+
         const additionalFiles = files['additionalFiles']
-            ? files['additionalFiles'].map((file) => ({ file: file.filename }))
+            ? files['additionalFiles'].map((file) => ({ file: shortenFileName(file.filename) }))
             : [];
         const {
             permit_country, permit_type, permit_srok, permit_doc_nom, permit_docstart, permit_docend,
@@ -136,7 +145,7 @@ const deleteLocalFormData = async (req, res) => {
 const saveFiles = async (req, res) => {
     try {
         const files = req.files.map((file) => ({
-            file: file.filename,
+            file: shortenFileName(file.filename),
         }));
         const formdata = await SaveFromVisa.findOne();
         const date = new Date();
